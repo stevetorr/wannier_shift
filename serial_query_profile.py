717aa079681c46cd2d4d793416f1ed4b7f328ac4
cs205 <cs205@dhcp-0169473138-7d-a2.client.student.harvard.edu>
2018-05-03 02:01:09 -0400

    Added to the profiling

--- serial_query_profile.py
+++ serial_query_profile.py
@@ -2,6 +2,8 @@ import numpy as np
 import time as time
 from multiprocessing import Pool
 
+import matplotlib.pyplot as plt
+
 """ 
 The point of this file is to profile in a semi-automatic way the comparative
 speed of querying all of the orbitals in the Wannier-coupling database in order to 
@@ -10,7 +12,7 @@ compare the querying process with and without spark.
 """
 
 
-def load_all_orbital_couplings_serial(k,l,thoroughness=1):
+def load_pair_orbital_couplings_serial(k,l,thoroughness=1):
     """
     For profiling purposes, try to perform a serial query of the relevant information.
     """
@@ -67,7 +69,7 @@ def load_orbitals_pool(arg_tuple):
     return load_orbitals_python(f=arg_tuple[0],k=arg_tuple[1],l=arg_tuple[2],debug=arg_tuple[3])
 
 
-def load_all_orbital_couplings_pool(k,l,thoroughness=1):
+def load_pair_orbital_couplings_pool(k,l,thoroughness=1):
 
     file_base = "new_proc_wan_"
     indices = range(0,400,thoroughness)
@@ -80,16 +82,69 @@ def load_all_orbital_couplings_pool(k,l,thoroughness=1):
     flat_data=[item for sublist in data_pieces for item in sublist]
 
     return np.array(flat_data)
+ 
+
+def load_all_orbital_couplings_serial(thoroughness=1):
+
+    file_base = 'new_proc_wan_'
+    indices = range(0,400,thoroughness)
+    file_names = [file_base+str(i) for i in indices]
+    
+    timer=True
+    debug=True
+    data_pieces=[]
+    all_lines=[]
+    for f in file_names:
+        if timer: t0=time.time()
+        if debug: print("Now reading in file",f)
+        with open(f,'r') as f_:
+            thelines=f_.readlines()[1:]
+            
+            thelines=[line.split(',') for line in thelines]
+
+            thelines=[ [float(x[n]) for n in [2,5,6,7,8,9]] for x in thelines]
+            all_lines+=thelines
+
+        if timer: print("Read file %s which took %f seconds" %(f, time.time()-t0))
+
+    if debug: print("returning array with dimension", np.array(all_lines).shape)
+    return(np.array(all_lines))
+
+
+    
     
 
-t0=time.time()
-ser=load_all_orbital_couplings_serial(1,1,25)
-t1=time.time()
-print("Total run took %f seconds" %(t1-t0))
+#TODO: ADD TO THIS
+load_all_orbital_couplings_serial(thoroughness=10)
+exit()
 
-t0=time.time()
-par=load_all_orbital_couplings_pool(1,1,25)
-print("Total parallel run took %f seconds" %(time.time()-t0))
+
+t0 = time.time()
+ser = load_pair_orbital_couplings_serial(1,1,1)
+t1 = time.time()
+print("Total serial run took %f seconds" %(t1-t0))
+
+tot_time_all = t1-t0
+
+t2 = time.time()
+ser=load_pair_orbital_couplings_serial(1,1,10)
+t3 = time.time()
+
+print("Abbreviated serial (only every 10th data set) run took %f seconds" %(t3-t2))
+
+tot_time_abb = t3-t2
+
+
+t4 = time.time()
+par = load_pair_orbital_couplings_pool(1,1,1)
+t5 = time.time()
+print("Total parallel run took %f seconds" %(t5-t4))
+
+t6 = time.time()
+par= load_pair_orbital_couplings_pool(1,1,10)
+t7=time.time()
+print("Abbreviated parallel run (only every 10th data set)  took %f seconds" %(t7-t6))
 
 print(par==ser)
+
 
